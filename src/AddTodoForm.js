57b6208e54a084d8719afbcc0208f6f70cedfe45
Med Amine ZAHER <mohammed-amine.zaher@capgemini.com>
2023-03-03 18:26:47 +0100

    externalize handle inputs functions in one function

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -5,20 +5,13 @@ export default function AddTodoForm({ createTodo }) {
   const [newTodo, setNewTodo] = useState(createdTodo);
   let [inputName, setInputName] = useState("");
   let [inputDescription, setInputDescription] = useState("");
-  const handleNameChange = (e) => {
-    setInputName(e.target.value);
-    createdTodo = { ...newTodo, name: e.target.value };
-    setNewTodo(createdTodo);
-  };
-  const handleDescriptionChange = (e) => {
-    createdTodo = { ...newTodo, description: e.target.value };
-    console.log(createdTodo);
-    setInputDescription(e.target.value);
+  const handleInputChange = (e, input, setInput) => {
+    setInput(e.target.value);
+    createdTodo = { ...newTodo, [input]: e.target.value };
     setNewTodo(createdTodo);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("handleSubmit", newTodo);
     inputName.length > 0 && createTodo([newTodo]);
     setInputName("");
     setInputDescription("");
@@ -32,7 +25,7 @@ export default function AddTodoForm({ createTodo }) {
         id="newTodo"
         name="newTodo"
         value={inputName}
-        onChange={handleNameChange}
+        onChange={(e) => handleInputChange(e, "name", setInputName)}
       />
       <input
         type={"text"}
@@ -40,7 +33,9 @@ export default function AddTodoForm({ createTodo }) {
         id="newTodo"
         name="newTodo"
         value={inputDescription}
-        onChange={handleDescriptionChange}
+        onChange={(e) =>
+          handleInputChange(e, "description", setInputDescription)
+        }
       />
       <button type="submit">Add Todo !</button>
     </form>
